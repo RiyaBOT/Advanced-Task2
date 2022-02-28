9229e1f08a71e373982b5e9abee399376ec408f5
NAME <EMAIL>
2022-02-28 18:57:22 +0800

    Initial

---
+++ OrderService/Models/OrderServiceContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
+
+
+
+namespace OrderService.Models
+{
+    public class OrderServiceContext : DbContext
+    {
+        public OrderServiceContext(DbContextOptions<OrderServiceContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<OrderServiceItem> OrderServiceItems { get; set; }
+    }
+}
